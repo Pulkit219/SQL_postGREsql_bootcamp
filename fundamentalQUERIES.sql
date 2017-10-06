7b5a214cc95c14b4a04405d73f0fbb279d330e9b
Pulkit219 <ascension1110@gmail.com>
2017-10-06 14:13:18 -0400

    1st commit

---
+++ fundamentalQUERIES.sql
@@ -0,0 +1,10 @@
+SELECT * from actor;
+SELECT DISTINCT rating from film;
+SELECT * FROM customer;
+SELECT * FROM payment;
+SELECT * FROM customer WHERE first_name ='Jared';
+SELECT  DISTINCT amount , payment_date FROM payment
+WHERE amount>= 0.99 OR amount<=5.00;
+SELECT * FROM customer WHERE first_name ='Jared';
+SELECT  DISTINCT amount , payment_date FROM payment
+WHERE amount>= 0.99 OR amount<=5.00;
