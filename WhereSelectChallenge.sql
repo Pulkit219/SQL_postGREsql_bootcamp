948b3969d6557b7bbeb730eb9bfdeacaab85f02b
Pulkit219 <ascension1110@gmail.com>
2017-10-07 00:11:20 -0400

    2nd comm
    WhereSelectChallenge

---
+++ WhereSelectChallenge.sql
@@ -0,0 +1,8 @@
+SELECT * FROM customer;
+SELECT email FROM customer WHERE first_name='Nancy' AND last_name='Thomas';
+
+SELECT * FROM film;
+SELECT title, description FROM film WHERE title='Outlaw Hanky';
+
+SELECT * FROM address;
+SELECT phone from address WHERE address='259 ipoh Drive';
