d36c7d9d49d0b96fdeeb60b01c227f107cac1ecf
Pulkit219 <ascension1110@gmail.com>
2017-10-07 23:14:41 -0400

    4th comm

--- countLimitOrderby.sql
+++ countLimitOrderby.sql
@@ -2,4 +2,7 @@ SELECT * FROM payment;
 SELECT COUNT(*) FROM payment;
 SELECT COUNT(DISTINCT amount) FROM payment;
 
+SELECT * FROM payment LIMIT 5;
+SELECT first_name, last_name FROM customer ORDER BY first_name, last_name DESC;
+SELECT first_name, last_name FROM customer ORDER BY first_name DESC;
 
