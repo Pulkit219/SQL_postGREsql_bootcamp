4b39eca848f18c77d420831f2a3474b5b5145874
Pulkit219 <ascension1110@gmail.com>
2017-10-16 00:22:29 -0400

    11th comm

--- JOIN.sql
+++ JOIN.sql
@@ -1,3 +1,11 @@
 --INNER JOIN
+
+
 SELECT customer.customer_id,first_name,last_name,email,amount,payment_date FROM customer INNER JOIN payment 
 ON payment.customer_id =customer.customer_id;
+
+SELECT customer.customer_id,first_name,last_name,email,amount,payment_date FROM customer INNER JOIN payment 
+ON payment.customer_id =customer.customer_id ORDER BY customer.customer_id;
+
+SELECT customer.customer_id,first_name,last_name,email,amount,payment_date FROM customer INNER JOIN payment 
+ON payment.customer_id =customer.customer_id WHERE first_name ILIKE 'A%';
