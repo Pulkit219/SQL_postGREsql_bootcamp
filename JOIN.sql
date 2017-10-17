48eeafbd8012dcbd9725159e935f8cf7b817ddda
Pulkit219 <ascension1110@gmail.com>
2017-10-17 14:19:05 -0400

    12th comm
    practicing JOINS

--- JOIN.sql
+++ JOIN.sql
@@ -9,3 +9,6 @@ ON payment.customer_id =customer.customer_id ORDER BY customer.customer_id;
 
 SELECT customer.customer_id,first_name,last_name,email,amount,payment_date FROM customer INNER JOIN payment 
 ON payment.customer_id =customer.customer_id WHERE first_name ILIKE 'A%';
+
+SELECT * FROM staff;
+SELECT * FROM payment;
