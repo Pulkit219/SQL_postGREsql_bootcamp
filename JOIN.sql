fbd7cfda330b1d1065a3d4034929cbf33b510af3
Pulkit219 <ascension1110@gmail.com>
2017-10-18 00:21:19 -0400

    13th comm

--- JOIN.sql
+++ JOIN.sql
@@ -12,3 +12,11 @@ ON payment.customer_id =customer.customer_id WHERE first_name ILIKE 'A%';
 
 SELECT * FROM staff;
 SELECT * FROM payment;
+
+SELECT payment_id,customer_id,amount,first_name,last_name FROM payment JOIN staff ON payment.staff_id=staff.staff_id;
+--USING AGGREGATE WITH JOINS
+SELECT first_name, SUM(amount),COUNT(first_name) FROM payment JOIN staff ON payment.staff_id=staff.staff_id GROUP BY first_name;
+--USING AGGREGATE WITH JOINS
+SELECT title,COUNT(title)AS copies  FROM inventory JOIN film on inventory.film_id=film.film_id WHERE store_id=1 GROUP BY title ORDER BY title;
+
+
