7d611a06d73bef0c817e239a2908c01c07fc3831
Pulkit219 <ascension1110@gmail.com>
2017-10-18 19:21:45 -0400

    14th comm

--- JOIN.sql
+++ JOIN.sql
@@ -19,4 +19,14 @@ SELECT first_name, SUM(amount),COUNT(first_name) FROM payment JOIN staff ON paym
 --USING AGGREGATE WITH JOINS
 SELECT title,COUNT(title)AS copies  FROM inventory JOIN film on inventory.film_id=film.film_id WHERE store_id=1 GROUP BY title ORDER BY title;
 
+SELECT title,name FROM film JOIN language ON film.language_id=language.language_id ;
+
+
+--TYPES OF JOIN INNER JOIN
+--OUTER JOIN=>join all props on both tables and return null on both sides if props doesn't match
+--LEFT OUTER join means left table side will not return null but right table will, while RIGHT OUTER JOIN is the opposite of it.
+
+--LEFT OUTER JOIN------------------------------------------
+SELECT title,film.film_id, inventory_id FROM film LEFT JOIN inventory ON film.film_id=inventory.film_id WHERE inventory_id IS null ORDER BY film.film_id;
+
 
