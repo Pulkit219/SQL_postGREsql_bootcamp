124f12e427aac926e9698a33e560dbde391ac972
Pulkit219 <ascension1110@gmail.com>
2017-11-17 23:57:01 -0500

    32nd comm

--- ASSESSMENT3.sql
+++ ASSESSMENT3.sql
@@ -7,7 +7,7 @@ student_id serial PRIMARY KEY,
     last_name VARCHAR(100) NOT NULL,
     homeroom_number integer NOT NULL,
     email VARCHAR(200) NOT NULL UNIQUE,
-    phone integer NOT NULL UNIQUE,
+    phone VARCHAR(50) NOT NULL UNIQUE,
     graduation_year integer
 );
 
@@ -19,5 +19,17 @@ teacher_id serial PRIMARY KEY,
     homeroom_number integer NOT NULL,
     department VARCHAR(200) NOT NULL,
     email VARCHAR(200) NOT NULL UNIQUE,
-    phone integer NOT NULL UNIQUE
+    phone VARCHAR(50) NOT NULL UNIQUE
 );
+
+	
+ALTER TABLE students ALTER COLUMN phone TYPE VARCHAR(50);
+ALTER TABLE teachers ALTER COLUMN phone TYPE VARCHAR(50);
+
+INSERT INTO students(first_name,last_name, 
+homeroom_number,phone,graduation_year,email)
+VALUES ('Mark','Watney',5,'7755551234',2035,'student@stu.com');
+
+INSERT INTO teachers(first_name,last_name, 
+homeroom_number,department,email,phone)
+VALUES ('Jonas','Salk',5,'Biology','jsalk@school.org','7755554321');
